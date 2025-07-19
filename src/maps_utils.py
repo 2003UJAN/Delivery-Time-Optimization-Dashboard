d5739dfe32e44e8467c6cf255f279d5ee4932883
Ujan Pradhan <113442287+2003UJAN@users.noreply.github.com>
2025-07-20 04:15:20 +0530

    Update maps_utils.py

--- src/maps_utils.py
+++ src/maps_utils.py
@@ -1,1 +1,9 @@
+import googlemaps
 
+def get_route_duration(gmaps_api_key, origin, destination):
+    gmaps = googlemaps.Client(key=gmaps_api_key)
+    result = gmaps.distance_matrix(origins=[origin],
+                                    destinations=[destination],
+                                    mode='driving')
+    duration = result['rows'][0]['elements'][0]['duration']['text']
+    return duration
